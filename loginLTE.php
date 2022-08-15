2145dddfc571c0bb8ed9b10abe4e1f446cb8ea95
Lucas Kaizer <74387751+kaizzer1@users.noreply.github.com>
2022-08-15 07:27:15 -0300

    commit LTE
    Commit LTE

--- loginLTE.php
+++ loginLTE.php
@@ -10,7 +10,7 @@ if(isset($_POST['sub'])){
    if(mysqli_num_rows($qu)>0){
       $f= mysqli_fetch_assoc($qu);
       $_SESSION['id']=$f['id'];
-      header ('location:index.php');
+      header ('location:indexLTE.php');
    }
    else{
        echo 'username or password does not exist';
